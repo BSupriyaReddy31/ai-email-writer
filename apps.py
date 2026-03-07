f931cfb344620efa829843917f828b785a08e26f
BSupriyaReddy31 <venkateshreddy3249@gmail.com>
2026-03-07 20:26:43 +0530

    Update apps.py

--- apps.py
+++ apps.py
@@ -96,7 +96,7 @@ custom_css = """
 
     /* 5. FIX TEXT VISIBILITY - Force labels and info boxes to be white */
     .stTextInput label, .stSelectbox label, .stTextArea label, .stApp p {
-        color: #00f0ff !important;
+        color: #110524 !important;
         font-size: 1rem !important;
     }
 
@@ -284,4 +284,5 @@ with tab2:
                 st.text_area(label="Draft", value=record['draft'], height=250, key=f"db_hist_{i}", label_visibility="collapsed")
     
     st.markdown("</div>", unsafe_allow_html=True)
+
 
